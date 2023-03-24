a4cd9c43755894acf2d816c798e72353cdbaa5b0
mustapha <mustamerak01@gmail.com>
2023-03-24 17:43:23 +0100

    add edge

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
+edge_list = [(1, 2), (2, 3), (3, 4), (3, 5), (4, 6),
+             (6, 7), (2, 8), (8, 9), (9, 4)]
+# G = nx.from_numpy_array(np.array([[0, 1, 0],
+#                                   [1, 0, 1],
+#                                   [1, 0, 0]]))
 
 G = nx.Graph()
-G.add_edge(1, 2)
-G.add_edge(2, 3, weight=0.9)
-G.add_edge("A", "B")
-G.add_edge("B", "B")
-G.add_node("C")
-G.add_node(print)
-
-nx.draw_spring(G, with_labels=True)
-
+G.add_edges_from(edge_list)
+# G = nx.complete_graph(5)
+print(dict(G.degree)[5])
+print(nx.shortest_path(G, 2, 4))
+nx.draw_planar(G, with_labels=True)
 plt.show()
+
+# nx.draw_planar(G, with_labels=True)
+# plt.show()
