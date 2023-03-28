df9557738c86ca6ca2d8054ab5ca621d085d6e77
mustapha <mustamerak01@gmail.com>
2023-03-28 11:47:58 +0100

    add matplotlib2

--- Matplotlib2.py
+++ Matplotlib2.py
@@ -1,26 +1,35 @@
+import csv
+import numpy as np
+import pandas as pd
+from collections import Counter
 from matplotlib import pyplot as plt
 
 plt.style.use("fivethirtyeight")
 
-ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+lang_responses = data['LanguagesWorkedWith']
 
-dev_y = [38496, 42000, 46752, 49320, 53200,
-         56000, 62316, 64928, 67317, 68748, 73752]
-plt.bar(ages_x, dev_y, color="#444444", label="All Devs")
+language_counter = Counter()
 
-py_dev_y = [45372, 48876, 53850, 57287, 63016,
-            65998, 70003, 70000, 71496, 75370, 83640]
-plt.bar(ages_x, py_dev_y, color="#008fd5", label="Python")
+for response in lang_responses:
+    language_counter.update(response.split(';'))
 
-js_dev_y = [37810, 43515, 46823, 49293, 53437,
-            56373, 62375, 66674, 68745, 68746, 74583]
-plt.bar(ages_x, js_dev_y, color="#e5ae38", label="JavaScript")
+languages = []
+popularity = []
 
-plt.legend()
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
 
-plt.title("Median Salary (USD) by Age")
-plt.xlabel("Ages")
-plt.ylabel("Median Salary (USD)")
+languages.reverse()
+popularity.reverse()
+
+plt.barh(languages, popularity)
+
+plt.title("Most Popular Languages")
+# plt.ylabel("Programming Languages")
+plt.xlabel("Number of People Who Use")
 
 plt.tight_layout()
 
